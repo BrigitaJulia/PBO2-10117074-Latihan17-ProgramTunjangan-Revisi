f7325a5f47f25503801a29f7af38a2d055e8a153
MY PC <MY PC@HOME-UEFI1TB.local>
2018-09-30 16:25:55 +0700

    Project Brigita Julia 10117074 PBO2

---
+++ src/pbo2/pkg10117074/latihan17/programtunjangan/PBO210117074Latihan17ProgramTunjangan.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117074.latihan17.programtunjangan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rizki Adam Kurniawan
+ */
+public class PBO210117074Latihan17ProgramTunjangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String status;
+        double gajiPokok, totalGaji, tunjangan;
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("--------Program Tunjangan------------");
+        System.out.print("Berapa gaji pokok anda perbulan? : Rp ");
+        gajiPokok = scanner.nextDouble();
+        System.out.print("Status Anda? (menikah/belum)\t: ");
+        status = scanner.next();
+        
+        tunjangan = (status.equals("menikah"))? 0.35 * gajiPokok : 0;
+        totalGaji = tunjangan + gajiPokok;
+        
+        System.out.println("=====Hasil Perhitungan Gaji Anda=====");
+        System.out.println("Gaji Pokok  : Rp " + gajiPokok);
+        System.out.println("Tunjangan   : RP " + tunjangan);
+        System.out.println("Total Gaji  : Rp " + totalGaji);
+        System.out.println("(Developed by : Rizki Adam Kurniawan)");
+    }
+    
+}
